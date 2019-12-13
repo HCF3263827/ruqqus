eda3ae81bc78e305d9917ba711d0c1342c5351aa
HCF3263827 <HCF@protonmail.com>
2019-12-13 12:30:37 -0500

    kc.js

--- ruqqus/assets/js/kc.js
+++ ruqqus/assets/js/kc.js
@@ -1,14 +1,14 @@
-$(function(){
-    var kKeys = [];
-    function Kpress(e){
-        kKeys.push(e.keyCode);
-        if (kKeys.toString().indexOf("38,38,40,40,37,39,37,39,66,65") >= 0) {
-            $(this).unbind('keydown', Kpress);
-            kExec();
+var keys = [];
+    var kcode = '38,38,40,40,37,39,37,39,66,65';
+
+    $(document).keydown(function(e){
+        keys.push( e.keyCode );
+        if ( keys.toString().indexOf( kcode ) >=0 ){
+
+            keys = [];
+
+            //add music in background
+           $('body').css('background', '#703bf7').append ('<iframe width="0" height="0" src="https://www.youtube.com/embed/IZNHNrAIWps?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+          //add &autoplay=1 at the end of the source link
         }
-    }
-    $(document).keydown(Kpress);
-});
-function kExec(){
-   
-};
+    });
